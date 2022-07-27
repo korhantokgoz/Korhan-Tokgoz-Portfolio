de49e37de8971cc597c6beb5fa59a13db6bf0626
korhantokgoz <korhan.tokgoz92@gmail.com>
2022-07-27 10:10:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =========================================================== typing animation ====================================================== */
+var typed = new Typed(".typing",{
+    strings: ["", "Full Stack Developer" ,  "Web Designer" , ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
